fe1552ffd49f5713cd8c090f0f067b46f6304c65
Ryan Schwab <rwschwab@ucsc.edu>
2023-08-03 11:42:47 -0700

    Customize location dropdown for UCSC

--- webapp/locationDropdown.js
+++ webapp/locationDropdown.js
@@ -1,7 +1,50 @@
-// START -- Enable user dropdown location validation -- added by K-State Libraires 06/2023
+// Location dropdown menu and validation
+// Code borrowed from Kansas State Libraries
+// https://github.com/kstatelibraries
+// Thank you rdeiser!
 
 $(document).ready(function(){
   var libraryObject = {
+    "McHenry": {
+      "mycur": "Child Curriculum",
+      "mycov": "Child Curriculum Oversize",
+      "mcheast": "East Asian Collection",
+      "mgcal": "Gov Pubs Cal Docs",
+      "mgcov": "Gov Pubs Cal Docs Oversize",
+      "mgmf": "Gov Pubs Microform",
+      "mgrce": "Gov Pubs Ref Census",
+      "mgus": "Gov Pubs US Docs",
+      "mguov": "Gov Pubs US Docs Oversize",
+      "mcper": "McHenry Current Journals",
+      "mheqp": "McHenry Equipment",
+      "mswest": "McHenry WEST",
+      "mprot": "McH Protect",
+      "mprov": "McH Protect Oversize",
+      "mref": "McH Ref",
+      "mratl": "McH Ref Atlas",
+      "mrdsk": "McH Ref Desk",
+      "mrovr": "McH Ref Oversize",
+      "mstax": "McH Stacks",
+      "mstov": "McH Stacks Oversize",
+      "mform": "Microform",
+      "mfgde": "Microform Guide",
+      "mnews": "Newspapers",
+      "mhres": "Reserves McH",
+    },
+    "S & E": {
+      "east": "East Asian Collection",
+      "scres": "Reserves S&E",
+      "scper": "S&E Current Journals",
+      "sequp": "S&E Equipment",
+      "sgame": "S&E Games",
+      "smaps": "S&E Maps",
+      "sovat": "S&E Oversize Atlas",
+      "scpro": "S&E Protect",
+      "sread": "S&E Reading",
+      "sstax": "S&E Stacks",
+      "sstov": "S&E Stacks Oversize",
+      "sswest": "S&E WEST",
+    },
   }
 
   window.onload = function () {
@@ -14,9 +57,9 @@ $(document).ready(function(){
     librarySel.onchange = function() {
       var selectedLibrary = libraryObject[this.value];
       locationSel.length = 1;
-      selectedLibrary.forEach(function(location) {
-        locationSel.options[locationSel.options.length] = new Option(location, location);
-      })
+      for (var x in selectedLibrary) {
+        locationSel.options[locationSel.options.length] = new Option(selectedLibrary[x], x);
+      }
     }
   }
 });
